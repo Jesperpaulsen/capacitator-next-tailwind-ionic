a652baeabcc4b65635abd454144080372b27360c
Jesper Paulsen <jesper@jesper.no>
2021-04-03 17:43:53 +0200

    add: capacitator, ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'no.jesper.capacitatorTest',
+  appName: 'capacitator-next-tailwind-ionic',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
